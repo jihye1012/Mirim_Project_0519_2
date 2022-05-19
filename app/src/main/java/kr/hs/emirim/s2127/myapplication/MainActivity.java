e2f4c3adc3006e392f9bccbcaca3e4314f7a6901
jihye1012 <yjh0510122@gmail.com>
2022-05-19 19:56:04 +0900

    시계

---
+++ app/src/main/java/kr/hs/emirim/s2127/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.s2127.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.time_datepicker);
+    }
+}
